ec588165e5293da090f05155fb978cb2ca181c72
Enrique NV <enriquen@live.de>
2020-09-17 08:48:28 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// Server code
 const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+
+let images = {};
+
+let boardImages = [
+    "https://s3.amazonaws.com/spicedling/jAVZmnxnZ-U95ap2-PLliFFF7TO0KqZm.jpg",
+    "https://s3.amazonaws.com/spicedling/wg8d94G_HrWdq7bU_2wT6Y6F3zrX-kej.jpg",
+    "https://s3.amazonaws.com/spicedling/XCv4AwJdm6QuzjenFPKJocpipRNNMwze.jpg",
+],
+
+app.get("/image-board", (req, res) => {
+    res.json({
+        boardImages,
+    });
+});
 
 app.listen(8080, () => console.log("listening"));
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,22 @@
+// Public javascript for frontend page
 (function () {
     new Vue({
         el: "main",
         data: {
             h1: "Image Board SPA",
             h2: "Latest images",
+            boardImages: [],
+        },
+        mounted: function () {
+            var that = this;
+            axios
+                .get("/image-board")
+                .then(function (resp) {
+                    that.boardImages = resp.data.boardImages;
+                })
+                .catch(function (err) {
+                    console.log("err in GET /image-board: ", err);
+                });
         },
     });
 })();
