b39efbd6fd097821f728256d2c7e2e652ac16f6d
Enrique NV <enriquen@live.de>
2020-09-17 09:23:03 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let boardImages = [
     "https://s3.amazonaws.com/spicedling/jAVZmnxnZ-U95ap2-PLliFFF7TO0KqZm.jpg",
     "https://s3.amazonaws.com/spicedling/wg8d94G_HrWdq7bU_2wT6Y6F3zrX-kej.jpg",
     "https://s3.amazonaws.com/spicedling/XCv4AwJdm6QuzjenFPKJocpipRNNMwze.jpg",
-],
+];
 
 app.get("/image-board", (req, res) => {
     res.json({
